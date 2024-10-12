010b054ecb05f20060277f12f8cce4d6ac5e6c4f
Alex Hall <AHallF11@gmail.com>
2024-10-11 20:37:48 -0600

    Final submission

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -5,6 +5,7 @@
 int main(int argc, char **argv) {
 
     openlog(NULL, 0, LOG_USER);
+    // printf("I'm starting to run the code.");
 
     if (argc < 3) {
         syslog(LOG_ERR, "Invalid Number of arguments.");
