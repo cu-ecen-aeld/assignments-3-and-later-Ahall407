1e73c28f3709283271513e67e8002f5f31bc5e68
Alex Hall <25327229+Ahall407@users.noreply.github.com>
2024-12-07 20:13:23 -0700

    final submission of assignment 4 part 1

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -13,7 +13,38 @@ void* threadfunc(void* thread_param)
 
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
-    //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+
+    int time_to_wait_sec = thread_func_args->wait_to_obtain_ms / 1000;
+    int time_to_release_sec = thread_func_args->wait_to_release_ms / 1000;
+
+    // Initial wait
+    sleep(time_to_wait_sec);
+
+    // obtain the mutex
+    int rc = pthread_mutex_lock(thread_func_args->mutex);
+    if ( rc != 0 ) {
+        perror("Failed to implement lock.");
+        thread_func_args->thread_complete_success = false;
+        pthread_exit(thread_param);
+    }
+
+    // wait to release the mutex
+    sleep(time_to_release_sec);
+
+    // release the mutex
+    rc = pthread_mutex_unlock(thread_func_args->mutex);
+    if ( rc != 0 ) {
+        perror("Failed to release mutex");
+        thread_func_args->thread_complete_success = false;
+        pthread_exit(thread_param);
+    }
+
+    // mark that the thread has successfully completed
+    thread_func_args->thread_complete_success = true;
+    // Exit the thread and return the thread data
+    pthread_exit(thread_param);
+
     return thread_param;
 }
 
@@ -28,6 +59,21 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
-}
+    struct thread_data *t_data; 
+    
+    t_data = (struct thread_data*) malloc(sizeof(struct thread_data));
 
+    t_data->wait_to_obtain_ms = wait_to_obtain_ms;
+    t_data->wait_to_release_ms = wait_to_release_ms;
+    t_data->mutex = mutex;
+    t_data->thread_complete_success = false;
+    
+    int rc = pthread_create(thread, NULL, threadfunc, t_data);
+    if ( rc != 0) {
+        perror("Failed to create pthread.");
+        free(t_data);
+        return false;
+    }
+
+    return true;
+}
