0baefdf51df78bbb535c3bafd629be0524c48406
Alex Hall <25327229+Ahall407@users.noreply.github.com>
2025-04-21 16:04:14 -0600

    final circular buffer implementation for assign7p1

--- aesd-char-driver/aesd-circular-buffer.c
+++ aesd-char-driver/aesd-circular-buffer.c
@@ -14,6 +14,7 @@
 #include <string.h>
 #endif
 
+#include <stdio.h>
 #include "aesd-circular-buffer.h"
 
 /**
@@ -29,9 +30,32 @@
 struct aesd_buffer_entry *aesd_circular_buffer_find_entry_offset_for_fpos(struct aesd_circular_buffer *buffer,
             size_t char_offset, size_t *entry_offset_byte_rtn )
 {
-    /**
-    * TODO: implement per description
-    */
+
+    size_t cumulative_offset = 0;
+    uint8_t index = buffer->out_offs;
+    uint8_t count;
+
+    if (buffer->full) {
+        count = AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED;
+    }
+    else if (buffer->in_offs >= buffer->out_offs){
+        count = buffer->in_offs - buffer->out_offs;
+    }
+    else {
+        count = AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED - buffer->out_offs + buffer->in_offs;
+    }
+
+    for (uint8_t i =0; i < count; i++){
+        struct aesd_buffer_entry *entry = &buffer->entry[index];
+        if (char_offset < cumulative_offset + entry->size) {
+            *entry_offset_byte_rtn = char_offset - cumulative_offset;
+            return entry;
+        }
+        cumulative_offset += entry->size;
+        index = (index + 1) % AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED;
+    }
+
+    // return NULL if out of range
     return NULL;
 }
 
@@ -44,9 +68,22 @@ struct aesd_buffer_entry *aesd_circular_buffer_find_entry_offset_for_fpos(struct
 */
 void aesd_circular_buffer_add_entry(struct aesd_circular_buffer *buffer, const struct aesd_buffer_entry *add_entry)
 {
-    /**
-    * TODO: implement per description
-    */
+
+    buffer->entry[buffer->in_offs] = *add_entry;
+
+    if (buffer->full) {
+        buffer->out_offs = (buffer->out_offs + 1) % AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED;
+    }
+
+    buffer->in_offs = (buffer->in_offs + 1) % AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED;
+
+    if (buffer->in_offs == buffer->out_offs) {
+        buffer->full = true;
+    }
+    else {
+        buffer->full = false;
+    }
+
 }
 
 /**
